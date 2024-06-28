6f2fdd91137e222e6fb2089a0414473e0a4e19b7
Christopher Huelitl <chrishuelitl03@gmail.com>
2024-06-27 19:21:22 -0500

    Working TicketMaster/Spotify usage + testing available with makefile

--- Back-End/main.py
+++ Back-End/main.py
@@ -1,5 +1,5 @@
 # Authored By: Joseph Arteaga
-
+# Co-Authored By: Christopher Huelitl
 # from flask import Flask, render_template
 # from flask import Flask, render_template, request, url_for, redirect, json, json
 
@@ -9,24 +9,28 @@ import base64
 def main():
     # Get Authorization for Spotify API
     spotify_access_token = getSpotifyAccessToken() 
+    ticketmaster_access_token = 'Y7AR2Y8hCu4MFHUa1acKZxWrvvvthY4d'
 
     artist_instances = []
-    # venue_instances = []
+    venue_instances = []
 
     # ISSUE: Genres in this list are not comprehensive to those used in artist_instances
     genre_instances = populateGenres(spotify_access_token)
 
     # Sample artists 
-    artist_names = ['Zach Bryan', 'Billie Eilish', 'Jason Aldean']
+    artist_names = ['Zach Bryan', 'Billie Eilish', 'Jason Aldean', 'Sabrina Carpenter']
 
     # Populate artist_instances with dictionaries of each artist
     for artist_name in artist_names:
         artist_id = getArtistId(spotify_access_token, artist_name)
         artist_instances += [getArtistInformation(artist_id, spotify_access_token)]
+        venue_instances += getEventsForArtist(ticketmaster_access_token, artist_name)
 
     # FOR DEBUGGING PURPOSES ONLY
     # for artist in artist_instances:
        # print(artist)
+
+    
     
 # Create access point to the Spotify API and return given access token
 def getSpotifyAccessToken():
@@ -36,12 +40,10 @@ def getSpotifyAccessToken():
 
     auth_str = f"{spotify_client_id}:{spotify_client_secret}"
     b64_auth_str = base64.b64encode(auth_str.encode()).decode()
-
     spotify_auth_url = 'https://accounts.spotify.com/api/token'
     b64_headers = {
         'Authorization': f'Basic {b64_auth_str}'
     }
-
     data = {
         'grant_type': 'client_credentials'
     }
@@ -138,6 +140,43 @@ def populateGenres(access_token):
         }
         genre_instances += [genre_instance]
     return genre_instances
+
+# Return a list of events for a particular artist
+def getEventsForArtist(access_token, artist_name) :
+    event_search_url = 'https://app.ticketmaster.com/discovery/v2/events.json'
+
+    params = {
+        'apikey': access_token,
+        'keyword': artist_name, 
+        'classificationName': 'music',
+        'locale': 'en-us', #filter only in the USA
+    }
+    response = (requests.get(event_search_url, params=params)).json()
+    events = []
+    for event in response['_embedded']['events']: 
+        address = event['_embedded']['venues'][0]['address']['line1']
+        address += f", {event['_embedded']['venues'][0]['city']['name']}"
+        #print("current address : " + str(address) + "\n")
+        address += f", {event['_embedded']['venues'][0]['state']['name']}"
+        #print("wanted address : " + str(address) + "\n")
+        salesDateRange = f"{event['sales']['public']['startDateTime']} to "
+        salesDateRange += event['sales']['public']['endDateTime']
+
+        event_instance = {
+            'event_id': event['id'],
+            'event_name': event['name'],
+            'artist_names': [artist_name],
+            'address': address,
+            'dateAndTime': event['dates']['start']['localDate'] if not (event['dates']['start']['dateTBD']) else 'TBD',
+            'salesStart-End': salesDateRange,
+            'priceRange': "TBD" if 'priceRanges' not in event else f"${event['priceRanges'][0]['min']} to ${event['priceRanges'][0]['max']}",
+            'genre': event['classifications'][0]['genre']['name'],
+            'venueName': event['_embedded']['venues'][0]['name'],
+            'ticketmasterURL': event['url']
+        }
+        events += [event_instance]
+
+    return events
 
 if __name__ == "__main__":
     main()
