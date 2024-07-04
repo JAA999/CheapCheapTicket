298104c5956e26ff826a8bd6e51ac4581ad960dc
hudsonwhipple <hudsonwhipple@utexas.edu>
2024-07-04 14:22:51 -0500

    small revision

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,8 +3,15 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h2>Home Page</h2>
-      <p>Welcome to the home page!</p>
+      <h2>Cheap Cheap Tickets</h2>
+      <p>Cheap tickets, Unforgettable Memories!</p>
+      <hr></hr>
+      <p> Start with one of the buttons below and begin exploring for the perfect events for you based on what is most important!</p>
+      <div class="d-grid gap-2 col-6 mx-auto">
+        <button class="btn btn-primary" type="button">Artists</button>
+        <button class="btn btn-primary" type="button">Genres</button>
+        <button class="btn btn-primary" type="button">Events</button>
+      </div>
     </div>
   );
 }
