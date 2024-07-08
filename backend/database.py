6644f3593934b17968f70eaaf7900b201f28c4b1
Joseph Arteaga <jarteaga999@icloud.com>
2024-07-08 16:42:16 -0500

    Updated to code from other branch

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 import json
-from models import app, db, Artist, Event, Genre
+from models import app, db, Artists, Events, Genres
 
 # Credit: Class Slides
 # Get the contents of a JSON file
@@ -9,12 +9,64 @@ def load_json_file(filename):
         fp.close()
     return jsn
 
-# def create_genres:
+def create_genres():
+    genres = load_json_file('genres.json')
 
-# def create_artists:
+    for genre in genres['Genres']:
+        i = Genres(
+            name=genre['name'],
+            id=genre['id'],
+            popular_artists=genre['popularArtists'],
+            upcoming_events=genre['upcomingEvents'],
+            top_songs=genre['topsongs'],
+            events_price_range=genre['eventsPriceRange']
+        )
+        db.session.add(i)
+    db.session.commit()
 
-# def create_events:
+def create_artists():
+    artists = load_json_file('artists.json')
 
-# create_genres()
-# create_artists()
-# create_events()
+    for artist in artists['Artists']:
+        i = Artists(
+            artist_name=artist['name'],
+            artist_id=artist['id'],
+            popularity=artist['popularity'],
+            albums=artist['albums'],
+            album_covers=artist['album_covers'],
+            future_events=artist['future_events'],
+            image_url=artist['image_url'],
+            genre_id=artist['genre_id']
+        )
+        db.session.add(i)
+    db.session.commit()
+
+
+def create_events():
+    events = load_json_file('events.json')
+
+    for event in events['Events']:
+        i = Events(
+            name=event['name'],
+            id=event["id"],
+            artist_names=event['artist_names'],
+            price_range=event['price_range'],
+            venue=event['venue'],
+            ticketmaster_URL=event['ticketmaster_URL'],
+            genre_id=event['genre_id']
+        )
+        db.session.add(i)
+    db.session.commit()
+
+db.drop_all()
+db.create_all()
+
+create_genres()
+create_artists()
+create_events()
+
+# This needs to be called every main.py is run
+
+# load each json by name
+#   call each method with the loaded data
+# then it is ready
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from database import app, db, create_events, create_artists, create_genres
-from models import Genre, Artist, Event
+from models import Genres, Artists, Events
 from data import get_gitlab_stats
 
 app = Flask(__name__)
@@ -22,35 +22,49 @@ def about_page():
 @app.route('/api/GetGenre/<string:genre_id>', methods=['POST'])
 def genres_page(genre_id):
     # When genres are listed as cards in grid view
-    if request.methods == 'GET':
-        genres = db.session.query('Genre').all()
+    if request.methods == 'GET': # is it request.method instead of methods
+        # genres = db.session.query('Genre').all()
+        genres = Genres.query.all()
         return jsonify([genre.to_dict() for genre in genres])
     elif request.methods == 'POST':
         # When user clicks on genre card (or name)
         data = request.json
-        if 'genre_id' in data:
+        if genre_id != '':
             # Search database based on genre_id
             genre_id = data['genre_id']
-            tgt = Genre.query.get(genre_id)
+            tgt = Genres.query.get(genre_id)
             return tgt.to_dict()
-        elif 'sort_by' in data:
-            return 
+
+
+@app.route('/api/GetGenres/<string:page>&<string:per_page>&<string:sort_by>&<string:sort_order>')
+def specific_genres(page, per_page, sort_by, sort_order):
+    if page != '':
+        page_num = int(page)
+        step = int(per_page) if not per_page else 5
+        genres = db.session.query('Genres').all()
+        return jsonify([(genres[page_num * step + i]).to_dict() for i in range(step + 1)])
 
 # Artists Page
 @app.route('/api/GetAllArtists/', methods=['GET'])
-@app.route('/api/GetArtist/<artist_id>', methods=['POST'])
+@app.route('/api/GetArtist/<string:artist_id>', methods=['POST'])
 def artists_page(artist_id):
     if request.methods == 'GET':
-        artists = db.session.query('Artist').all()
+        artists = db.session.query('Artists').all()
         return jsonify([artist.to_dict() for artist in artists])
     elif request.methods == 'POST':
         data = request.json
         if artist_id != '':
-            tgt = Artist.query.get(artist_id)
+            tgt = Artists.query.get(artist_id)
             return tgt.to_dict()
-        elif 'sort_by' in data:
-            return
 
+@app.route('/api/GetArtists/<string:page>&<string:per_page>&<string:sort_by>&<string:sort_order>')
+def specific_artists(page, per_page, sort_by, sort_order):
+    if page != '':
+        page_num = int(page)
+        step = int(per_page) if not per_page else 20
+        artists = db.session.query('Artists').all()
+        return jsonify([(artists[page_num * step + i]).to_dict() for i in range(step + 1)])
+            
 # Get list of genre instances
     # Iterate through list of instances
         # for each instance iterate thorugh popularArtistNames
@@ -63,14 +77,20 @@ def artists_page(artist_id):
 @app.route('/api/GetEvent/<string:event_id>', methods=['POST'])
 def events_page(event_id):
     if request.methods == 'GET':
-        events = db.session.query('Event').all()
+        events = db.session.query('Events').all()
         return jsonify([event.to_dict() for event in events])
     elif request.methods == 'POST':
         data = request.json
         if event_id != '':
-            tgt = Event.query.get(event_id)
+            tgt = Events.query.get(event_id)
             return tgt.to_dict()
-        elif 'sort_by' in data:
-            return
+
+@app.route('/api/GetEvents/<string:page>&<string:per_page>&<string:sort_by>&<string:sort_order>')
+def specific_events(page, per_page, sort_by, sort_order):
+    if page != '':
+        page_num = int(page)
+        step = int(per_page) if not per_page else 20
+        events = db.session.query('Events').all()
+        return jsonify([(events[page_num * step + i]).to_dict() for i in range(step + 1)])
 
 
